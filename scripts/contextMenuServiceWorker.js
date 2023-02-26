209f0163de8d465e631e3f7807f748d123bdff16
rallao <rallao85@gmail.com>
2023-02-26 01:16:23 -0300

    added getKey and generate functions

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,4 +1,39 @@
-// New function here
+const getKey = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(["openai-key"], (result) => {
+      if (result["openai-key"]) {
+        const decodedKey = atob(result["openai-key"]);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+
+const generate = async (prompt) => {
+  // Get your API key from storage
+  const key = await getKey();
+  const url = "https://api.openai.com/v1/completions";
+
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1250,
+      temperature: 0.7,
+    }),
+  });
+
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+};
+
 const generateCompletionAction = async (info) => {
   try {
     const { selectionText } = info;
