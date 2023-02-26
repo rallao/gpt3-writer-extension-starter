107de8ff4ab711919bcc8012877bc79277bf62f3
rallao <rallao85@gmail.com>
2023-02-26 18:35:14 -0300

    added generateCompleteAction funcitons

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -53,22 +53,39 @@ const generate = async (prompt) => {
 
 const generateCompletionAction = async (info) => {
   try {
+    // Send mesage with generating text (this will be like a loading indicator)
+    sendMessage("generating...");
+
     const { selectionText } = info;
     const basePromptPrefix = `
-      Write me a detailed table of contents for a blog post with the title below.
+        Write me a detailed table of contents for a blog post with the title below.
+        
+        Title:
+        `;
 
-      Title:
-      `;
-
-    // Add this to call GPT-3
     const baseCompletion = await generate(
       `${basePromptPrefix}${selectionText}`
     );
 
-    // Let's see what we get!
-    console.log(baseCompletion.text);
+    const secondPrompt = `
+          Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+          
+          Title: ${selectionText}
+          
+          Table of Contents: ${baseCompletion.text}
+          
+          Blog Post:
+            `;
+
+    const secondPromptCompletion = await generate(secondPrompt);
+
+    // Send the output when we're all done
+    sendMessage(secondPromptCompletion.text);
   } catch (error) {
     console.log(error);
+
+    // Add this here as well to see if we run into any errors!
+    sendMessage(error.toString());
   }
 };
 
